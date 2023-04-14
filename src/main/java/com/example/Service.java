d8773265845d02bd3a11d240ab9fccf13b6cc6ca
elena.ermakova <alena.ermakova2004@gmail.com>
2023-04-15 00:03:18 +0300

    add simple classes

---
+++ src/main/java/com/example/Service.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Service {
+    Product whiteRose = new Rose(1, "rosemary", 300, "white", true);
+    Product redRose = new Rose(2, "rosemary", 350, "red", true);
+    Menu menu = new Menu();
+}
