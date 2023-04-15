5470d45bde2c5324c41c313c72a3868728170495
Alina Raznosilina <raznosilina.av@phystech.edu>
2023-04-15 15:43:50 +0000

    Create spring project

--- src/main/java/com/example/Service.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class Service {
-    Product whiteRose = new Rose(1, "rosemary", 300, "white", true);
-    Product redRose = new Rose(2, "rosemary", 350, "red", true);
-    Menu menu = new Menu();
-}
