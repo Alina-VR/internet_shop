48e00cc8c0a43b40974aebe74cf6f34986104968
Alina Raznosilina <raznosilina.av@phystech.edu>
2023-04-16 10:39:08 +0000

    Add base

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,8 +5,10 @@ import jakarta.servlet.ServletException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class Main extends SpringBootServletInitializer {
 	public static void main(String[] args){
 		SpringApplication.run(Main.class, args);
---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE countries (id INT,name
+ VARCHAR(20));
+INSERT INTO countries VALUES (1, 'USA');
+INSERT INTO countries VALUES (2, 'France');
+INSERT INTO countries VALUES (3, 'Brazil');
+INSERT INTO countries VALUES (4, 'Italy');
+INSERT INTO countries VALUES (5, 'Canada');
