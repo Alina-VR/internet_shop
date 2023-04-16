92e43425139b011114fc55b06f0f9c1f1c33e1b4
alina.raznosilina <raznosilina.av@phystech.edu>
2023-04-16 16:44:18 +0300

    fix: data base

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,3 +1,5 @@
+CREATE TABLE countries (id INT,name
+ VARCHAR(20));
 INSERT INTO countries (id, name) VALUES (1, 'USA');
 INSERT INTO countries (id, name) VALUES (2, 'France');
 INSERT INTO countries (id, name) VALUES (3, 'Brazil');
