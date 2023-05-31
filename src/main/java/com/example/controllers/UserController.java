281a41d07bcf1c1987552dbe4e6b12e3f06b73a1
alina.raznosilina <raznosilina.av@phystech.edu>
2023-05-31 15:53:46 +0300

    feat: UserController, not working version

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -2,15 +2,21 @@ package com.example.controllers;
 
 import com.example.models.User;
 import com.example.services.UserService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/login")
     public String login() {
@@ -18,19 +24,49 @@ public class UserController {
     }
 
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
         return "registration";
     }
 
-
     @PostMapping("/registration")
-    public String createUser(User user) {
-        userService.createUser(user);
-        return "redirect:/login";
+    public String addUser(@ModelAttribute("userForm") @Valid User userForm, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())) {
+            model.addAttribute("passwordError", "Пароли не совпадают");
+            return "registration";
+        }
+        if (!userService.createUser(userForm)) {
+            model.addAttribute("usernameError", "Пользователь с таким именем уже существует");
+            return "registration:/login";
+        }
+
+        return "redirect:/";
     }
 
-    @GetMapping("/hello")
-    public String securityUrl() {
-        return "hello";
-    }
+    //    @GetMapping("/login")
+    //    public String login() {
+    //        return "login";
+    //    }
+    //
+    //    @GetMapping("/registration")
+    //    public String registration() {
+    //        return "registration";
+    //    }
+    //
+    //
+    //    @PostMapping("/registration")
+    //    public String createUser(User user) {
+    //        userService.createUser(user);
+    //        return "redirect:/login";
+    //    }
+    //
+    //    @GetMapping("/hello")
+    //    public String securityUrl() {
+    //        return "hello";
+    //    }
 }
--- src/main/java/com/example/models/User.java
+++ src/main/java/com/example/models/User.java
@@ -4,15 +4,31 @@ package com.example.models;
 
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
-
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -30,20 +46,20 @@ public class User implements UserDetails {
     private String name;
     @Column(name = "active")
     private boolean active;
-//    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-//    @JoinColumn(name = "image_id")
-//    private Image avatar;
+    //    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    //    @JoinColumn(name = "image_id")
+    //    private Image avatar;
     @Column(name = "password", length = 1000)
     private String password;
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_role",
-        joinColumns = @JoinColumn(name = "user_id"))
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>();
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
     private List<Product> products = new ArrayList<>();
     private LocalDateTime dateOfCreated;
-
+    @Transient
+    private String passwordConfirm;
 
     @PrePersist
     private void init() {
@@ -51,11 +67,11 @@ public class User implements UserDetails {
     }
 
     // security
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() { //TODO: не поняла, что с этим делать, класс role не видит, в принципе нужно секьюрити
-//        return roles;
-//    }
+    //
+    //    @Override
+    //    public Collection<? extends GrantedAuthority> getAuthorities() { //TODO: не поняла, что с этим делать, класс role не видит, в принципе нужно секьюрити
+    //        return roles;
+    //    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -67,23 +83,30 @@ public class User implements UserDetails {
         return email;
     }
 
-  @Override
+    @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
-   @Override
+    @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
-  @Override
+    @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
-  @Override
+    @Override
     public boolean isEnabled() {
         return active;
+    }
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
     }
 }
